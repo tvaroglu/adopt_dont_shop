0e96b5c2c55bd5bcaaf90a8406a466a21ce3b37a
Taylor Varoglu <tava8398@colorado.edu>
2021-07-20 16:18:31 -0600

    Add PetApplication#update_status to models and spec

--- app/models/pet_application.rb
+++ app/models/pet_application.rb
@@ -2,4 +2,9 @@ class PetApplication < ApplicationRecord
   belongs_to :pet
   belongs_to :application
 
+  def self.update_status(id, new_status)
+    application = PetApplication.find(id)
+    application.update(status: new_status)
+  end
+
 end
--- spec/models/pet_application_spec.rb
+++ spec/models/pet_application_spec.rb
@@ -5,4 +5,44 @@ RSpec.describe PetApplication, type: :model do
     it {should belong_to :pet}
     it {should belong_to :application}
   end
+
+  describe "class methods" do
+    it 'can #update_status independent of Application status' do
+      application = Application.create!(
+        applicant_fullname: 'John Smith',
+        applicant_address: '1200 3rd St.',
+        applicant_city: 'Golden',
+        applicant_state: 'CO',
+        applicant_zipcode: '80401',
+        applicant_description: 'I am a good guy',
+        status: 'Pending')
+
+      shelter = Shelter.create!(
+        name: 'Aurora Shelter',
+        address: '123 Main St.',
+        city: 'Aurora',
+        state: 'CO',
+        zipcode: '80010',
+        foster_program: false,
+        rank: 9)
+      shelter.pets.create!(
+        name: 'Mr. Pirate',
+        breed: 'Tuxedo Shorthair',
+        age: 5,
+        adoptable: true)
+
+      application.pets << shelter.pets.all.first
+      pet_application = PetApplication.find_by(application_id: application.id)
+
+      expect(pet_application.status).to eq(nil)
+      expect(application.status).to eq('Pending')
+
+      PetApplication.update_status(pet_application.id, 'Approved')
+      updated_pet_application = PetApplication.find_by(application_id: application.id)
+
+      expect(updated_pet_application.status).to eq('Approved')
+      expect(application.status).to eq('Pending')
+    end
+  end
+
 end
