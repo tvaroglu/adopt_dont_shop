53f7cc6805908c3314458c5a46a8b6695a5d6e9a
Taylor Varoglu <tava8398@colorado.edu>
2021-07-21 12:27:32 -0600

    Add PetApplication#reject to models and spec

--- app/models/pet_application.rb
+++ app/models/pet_application.rb
@@ -3,15 +3,20 @@ class PetApplication < ApplicationRecord
   belongs_to :application
 
   def self.update_application_status(app_id, pet_id, new_status)
-    self.where(application_id: app_id, pet_id: pet_id)
+    where(application_id: app_id, pet_id: pet_id)
     .first
     .update(status: new_status)
+    self.reject(app_id) if new_status == 'Rejected'
+  end
+
+  def self.reject(app_id)
+    Application.find_by(id: app_id)
+    .update(status: 'Rejected')
   end
 
   def self.pet_approval_status(pet_id, application_id)
-    self.select(:status)
-    .where(pet_id: pet_id)
-    .where(application_id: application_id)
+    select(:status)
+    .where(pet_id: pet_id, application_id: application_id)
     .first
   end
 
--- spec/models/pet_application_spec.rb
+++ spec/models/pet_application_spec.rb
@@ -41,7 +41,7 @@ RSpec.describe PetApplication, type: :model do
       pet_application = PetApplication.find_by(application_id: application.id)
 
       expect(pet_application.status).to eq('Approved')
-      expect(application.status).to eq('Pending')
+      expect(Application.find(application.id).status).to eq('Pending')
     end
     it 'can return #pet_approval_status independent of Application status' do
       application = Application.create!(
@@ -78,7 +78,7 @@ RSpec.describe PetApplication, type: :model do
       pet_application = PetApplication.pet_approval_status(application.pets.first.id, application.id)
 
       expect(pet_application.status).to eq('Pending Review')
-      expect(application.status).to eq('Pending')
+      expect(Application.find(application.id).status).to eq('Pending')
     end
 
     it '#pet_approval_status is independent of individual Application statuses' do
@@ -133,9 +133,53 @@ RSpec.describe PetApplication, type: :model do
             PetApplication.pet_approval_status(
               shelter.pets.first.id, application_1.id))
 
-      expect(application_1.status).to eq('Pending')
-      expect(application_2.status).to eq('In Progress')
+      expect(Application.find(application_1.id).status).to eq('Rejected')
+      expect(Application.find(application_2.id).status).to eq('In Progress')
     end
+  end
+
+  it '#pet_approval_status of "Rejected" will update the corresponding Application status to "Rejected"' do
+    application = Application.create!(
+      applicant_fullname: 'John Smith',
+      applicant_address: '1200 3rd St.',
+      applicant_city: 'Golden',
+      applicant_state: 'CO',
+      applicant_zipcode: '80401',
+      applicant_description: 'I am a good guy',
+      status: 'Pending')
+
+    shelter = Shelter.create!(
+      name: 'Aurora Shelter',
+      address: '123 Main St.',
+      city: 'Aurora',
+      state: 'CO',
+      zipcode: '80010',
+      foster_program: false,
+      rank: 9)
+    shelter.pets.create!(
+      name: 'Mr. Pirate',
+      breed: 'Tuxedo Shorthair',
+      age: 5,
+      adoptable: true)
+    shelter.pets.create!(
+      name: 'Macaroni',
+      breed: 'Scottish Fold',
+      age: 2,
+      adoptable: true)
+
+    application.pets << shelter.pets.all.first
+    application.pets << shelter.pets.all.last
+
+    expected = Application.find(application.id)
+    expect(expected.status).to eq('Pending')
+
+    PetApplication.update_application_status(application.id, shelter.pets.first.id, 'Approved')
+    expected = Application.find(application.id)
+    expect(expected.status).to eq('Pending')
+
+    PetApplication.update_application_status(application.id, shelter.pets.last.id, 'Rejected')
+    expected = Application.find(application.id)
+    expect(expected.status).to eq('Rejected')
   end
 
 end
